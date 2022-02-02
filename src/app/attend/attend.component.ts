29012616e43b64c09d90bcd1b2c4d0ea3d530a15
Jacob Ferrero <jacobaferrero@gmail.com>
2022-02-01 22:01:00 -0600

    Attend screen updates

--- src/app/attend/attend.component.ts
+++ src/app/attend/attend.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {UserService} from "../user.service";
 import {MapService, PlaceResult} from "../map.service";
 import {delay, Subscription} from "rxjs";
+import {format, isToday, isTomorrow, startOfToday} from "date-fns";
 
 @Component({
   selector: 'app-attend',
@@ -10,29 +11,39 @@ import {delay, Subscription} from "rxjs";
 })
 export class AttendComponent implements OnInit {
 
-  suggestions = [
-    {
-      id: '1',
-      count: 2,
-    }, {
-      id: '2',
-      count: 1,
-    }
-  ]
+  suggestions = [] as Array<Suggestion>
 
   meetChosen?: string
   timeIsExpired = false
   meetHasAlreadyBegun = false
 
+  days = [] as Array<DayOption>
   search = ''
+  searchDate!: number
+  searchName = ''
+  searchGeo = [] as Array<number>
+  searchAddress = ''
   searchMapImg = ''
+  searchTime = '12:00'
   placeSuggestions = [] as Array<PlaceResult>
   private autocompleteSub?: Subscription
 
   constructor(public user: UserService, private map: MapService, private cr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.days = [0, 1, 2, 3, 4, 5, 6].map(x => {
+      const m = new Date(startOfToday().getTime() + x * 86400000)
+
+      return {
+        time: m.getTime(),
+        text: this.fmt(m)
+      }
+    }).filter(x => {
+      return !isToday(x.time) || new Date().getHours() < 18
+    })
+    this.searchDate = this.days[0].time
   }
+
 
   expired() {
     this.timeIsExpired = true
@@ -40,10 +51,18 @@ export class AttendComponent implements OnInit {
   }
 
   addMeetSuggestion() {
+    const d = new Date(Number(this.searchDate))
+    d.setHours(Number(this.searchTime.split(':')[0]))
+    d.setMinutes(Number(this.searchTime.split(':')[1]))
+    const date = `${format(d, 'h:mm a')}, ${this.fmt(d)}`
     this.suggestions.push(
       {
-        id: '3',
-        count: 1,
+        id: Math.random().toString(),
+        attendees: 2,
+        date,
+        name: this.searchName,
+        address: this.searchAddress,
+        geo: this.searchGeo
       }
     )
 
@@ -62,6 +81,33 @@ export class AttendComponent implements OnInit {
 
   chooseAutocomplete(result: PlaceResult) {
     this.searchMapImg = this.map.staticMap(result.geo.join(','))
+    this.searchName = result.name
+    this.searchAddress = result.address!
+    this.searchGeo = result.geo
     this.placeSuggestions.length = 0
+    this.search = ''
   }
+
+  img(geo: Array<number>): string {
+    return this.searchMapImg = this.map.staticMap(geo.join(','))
+  }
+
+  private fmt(date: Date): string {
+    const t = isToday(date) ? 'Today' : isTomorrow(date) ? 'Tomorrow' : ''
+    return t + format(date, `${ t ? '' : 'EEEE' }, MMMM do`)
+  }
+}
+
+interface DayOption {
+  time: number
+  text: string
+}
+
+interface Suggestion {
+  id: string
+  name: string
+  address: string
+  date: string
+  attendees: number
+  geo: Array<number>
 }
